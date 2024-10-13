228b74a14b22bab44f9ae05466a27b8432bc98ab
ShiroKitsu <93395807+Bercik1287@users.noreply.github.com>
2024-10-13 19:13:02 +0200

    Lista
    dodanie listy dwukierunkowej

--- lista.cpp
+++ lista.cpp
@@ -17,16 +17,8 @@ private:
     
 public:
     ListaDwukierunkowa() : przod(nullptr), tyl(nullptr) {}
-    void dodajZPrzodu(int wartosc) {
-        Wezel* Wezel2 = new Wezel(wartosc);
-        if (!przod) {
-            przod = tyl = Wezel2;
-        } else {
-            Wezel2->nastepny = przod;
-            przod->poprzedni = Wezel2;
-            przod = Wezel2;
-        }
-    }
+    
+}
 
 int main() {
     
